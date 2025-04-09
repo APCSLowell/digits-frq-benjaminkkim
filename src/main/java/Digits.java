7239826a84546b0467bf61fbbe96a2bca4275d8c
benjaminkkim <bekim@s.sfusd.edu>
2025-04-09 12:12:56 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -14,7 +14,7 @@ public class Digits
 	}
 
 	public boolean isStrictlyIncreasing() {
-		for(int i = 0; i < digitList.size(); i++) {
+		for(int i = 0; i < digitList.size() - 1; i++) {
 			if(digitList.get(i+1) <= digitList.get(i)) {
 				return false;
 			}
