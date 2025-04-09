43bb9c9245346b1c48abf6ce2f90b6009418c635
benjaminkkim <bekim@s.sfusd.edu>
2025-04-09 11:39:52 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -5,15 +5,28 @@ public class Digits
 
 	private ArrayList<Integer> digitList;
 
-	public Digits(int num)
-	{ /* to be implemented in part (a) */ 
-	    
+	public Digits(int num) {
+		String numtoString = toString(num);
+		int count = 0;
+		for(int i = 0; i < numtoString.length(); i++) {
+			count++;
+		}
+		int start = 0;
+		int end = 1;
+		for(int j = 0; j < count; j++) {
+			digitList.add(numtoString.substring(start, end));
+			start++;
+			end++;
+		}
 	}
 
-	public boolean isStrictlyIncreasing()
-	{ /* to be implemented in part (b) */
-		
-
+	public boolean isStrictlyIncreasing() {
+		for(int i = 0; i < digitList.size(); i++) {
+			if(digitList.get(i+1) <= digitList.get(i)) {
+				return false;
+			}
+		}
+		return true;
 	}
 	
 	public String toString()
